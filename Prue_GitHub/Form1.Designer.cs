6631abb7699639e38d27569055044b07a6e44446
Lucia <bernallucia80@gmail.com>
2023-08-09 10:08:44 -0500

    Todo Melo

--- Prue_GitHub/Form1.Designer.cs
+++ Prue_GitHub/Form1.Designer.cs
@@ -32,11 +32,12 @@ namespace Prue_GitHub
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(86, 219);
+            this.button1.Location = new System.Drawing.Point(63, 139);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 37);
             this.button1.TabIndex = 0;
@@ -46,7 +47,7 @@ namespace Prue_GitHub
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(43, 65);
+            this.comboBox1.Location = new System.Drawing.Point(43, 45);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(186, 21);
             this.comboBox1.TabIndex = 1;
@@ -54,22 +55,33 @@ namespace Prue_GitHub
             // checkedListBox1
             // 
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(33, 92);
+            this.checkedListBox1.Location = new System.Drawing.Point(71, 93);
             this.checkedListBox1.Name = "checkedListBox1";
             this.checkedListBox1.Size = new System.Drawing.Size(135, 19);
             this.checkedListBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Olacaradebola";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 268);
+            this.ClientSize = new System.Drawing.Size(292, 219);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -78,6 +90,7 @@ namespace Prue_GitHub
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
